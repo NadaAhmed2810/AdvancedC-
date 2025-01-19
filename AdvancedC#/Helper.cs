b7d0b39b125be161858a94eeb7e862da882f73cc
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-19 20:54:22 +0200

    Generics01

---
+++ AdvancedC#/Helper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedC_
+{
+    static class Helper<T>
+    {
+        #region incorrect usage 
+        //public static void Swap(ref int x, ref int y)
+        //{
+        //    Console.WriteLine("_______________Swap__________________");
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+
+        //}
+        //public static void Swap(ref double x, ref double y)
+        //{
+        //    Console.WriteLine("_______________Swap__________________");
+        //    double temp = x;
+        //    x = y;
+        //    y = temp;
+
+        //}
+        //public static void Swap(ref Point x, ref Point y)
+        //{
+        //    Console.WriteLine("_______________Swap__________________");
+        //    Point temp = x;
+        //    x = y;
+        //    y = temp;
+
+        //} 
+        #endregion
+        ///many boxing and unboxing 
+        //public static void Swap(ref object x,ref object y)
+        //{
+        //    Console.WriteLine("_______________Swap__________________");
+        //    object  temp = x;
+        //    x = y;
+        //    y = temp;
+
+        //}
+
+        public static void Swap(ref T x, ref T y)
+        {
+            Console.WriteLine("_______________Swap__________________");
+            T temp = x;
+            x = y;
+            y = temp;
+
+        }
+        public static void Print<T>(string s)
+        {
+            Console.WriteLine($"{s}");
+        }
+
+
+    }
+}
--- AdvancedC#/Program.cs
+++ AdvancedC#/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Generics01
+            //int A = 4, B = 3;
+
+            //Console.WriteLine($"A:{A}");
+            //Console.WriteLine($"B:{B}");
+            //Helper<int>.Swap(ref A, ref B);
+            //Console.WriteLine($"A:{A}");
+            //Console.WriteLine($"B:{B}");
+            //double X = 3.7, Y = 2.5;
+            //Console.WriteLine($"X:{X}");
+            //Console.WriteLine($"Y:{Y}");
+            //Helper.Swap(ref X, ref Y);
+            //Console.WriteLine($"X:{X}");
+            //Console.WriteLine($"Y:{Y}");
+            //Point point1 = new Point(10, 20);
+            //Point point2 = new Point(11, 22);
+            //Console.WriteLine(point1.ToString());
+            //Console.WriteLine(point2.ToString());
+            //Helper.Swap(ref point1, ref point2);
+            //Console.WriteLine(point1.ToString());
+            //Console.WriteLine(point2.ToString());
+
+            #endregion
         }
     }
 }
