67dafa2cceb190179487d3045b1a40d3067e04f6
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-19 19:51:45 +0200

    Add project files.

---
+++ AdvancedC#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdvancedC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
