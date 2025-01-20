7256d3baf31eabec2097edc292b782b98e004fec
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-20 13:48:17 +0200

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignment
+﻿using Assignment.Question2;
+
+namespace Assignment
 {
     internal class Program
     {
@@ -14,6 +16,16 @@
             //    Console.WriteLine(arr[i]);
             //}
             #endregion
+            #region Q2:Range Class
+            //var R01 = new Range<int>(30, 20);
+            //Console.WriteLine(R01.IsInRange(0));
+            //Console.WriteLine($"Range:{R01.Length()}");
+            //Console.WriteLine("___________________________");
+            //var R02 = new Range<double>(-100.5, 100.75);
+            //Console.WriteLine(R01.IsInRange(0));
+            //Console.WriteLine($"Range:{R02.Length()}");
+            #endregion
+
         }
     }
 }
